f45d0784e713e91fecb29a04293ae0d83c79863b
agne burokaite <agne.burokaite@gmail.com>
2018-11-06 08:11:36 +0200

    sdvc

--- skriptas.js
+++ skriptas.js
@@ -1,3 +1,18 @@
+var a;
+var b;
+var c;
+var y=Math.sqrt(4);
 function myFunction() {
    document.getElementById("demo").innerHTML = "Paragraph changed.";
+	a=1;
+}
+function back() {
+   document.getElementById("demo").innerHTML = "no.";
+	b=5;
+}
+function plus() {
+
+
+document.getElementById("demo").innerHTML = y;
+
 }
